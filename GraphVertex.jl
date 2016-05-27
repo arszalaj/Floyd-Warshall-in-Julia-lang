fbf6651c45af2c431ce8967edfb16c68b1120811
arszalaj <arszalaj@gmail.com>
2016-05-27 14:09:54 +0200

    Add files via upload

---
+++ GraphVertex.jl
@@ -0,0 +1,36 @@
+type Wierzcholek
+    id::Int
+    value::ASCIIString
+
+    Wierzcholek(x,y) = new(x,y)
+end
+
+type Krawedz
+    id::Int
+    v1::Wierzcholek
+    v2::Wierzcholek
+    weight::Int
+
+    Krawedz(v,x,y,z) = new(v,x,y,z)
+end
+
+function iteracja(vertex::Wierzcholek)
+  for i=1:1:length(vertex)
+    println(vertex[i].id);
+  end
+
+  return true;
+end
+
+function iteracja_krawedzi(edge_list::Krawedz{})
+
+    println(edge_list.id);
+
+
+  return true;
+end
+
+# @test iteracja([1,2]) == true
+
+println("Test zakończony sukcesem");
+# @test iteracja_krawedzi([Krawedz(1,Wierzcholek(1,"a"),Wierzcholek(2,"b"),1), Krawedz(1,Wierzcholek(1,"a"),Wierzcholek(2,"b"),1)]) == true
---
+++ test.jl
@@ -0,0 +1,12 @@
+using Base.Test
+
+# @test GraphVertex(1,"a").id == 1
+vertex1 = Wierzcholek(1,"a");
+vertex2 = Wierzcholek(2,"b");
+
+# @test iteracja([1,2,3]) == true
+@test Krawedz(1,Wierzcholek(1,"a"),Wierzcholek(2,"b"),1).v2.value == "b"
+krawedz1 = Krawedz(1,Wierzcholek(1,"a"),Wierzcholek(2,"b"),1);
+krawedz2 = Krawedz(2,Wierzcholek(2,"b"),Wierzcholek(1,"a"),1);
+@test iteracja_krawedzi(krawedz1, krawedz2) == true
+println("Test zakończony sukcesem");
