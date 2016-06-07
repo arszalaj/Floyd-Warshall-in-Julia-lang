680d708be3656ecebb1f01c0163c88ed2ea04fad
Arkadiusz Szałaj <arkadiusz.szalaj@comp.com.pl>
2016-06-07 20:50:25 +0200

    is_neighbour() function introduced

--- GraphVertex.jl
+++ GraphVertex.jl
@@ -22,15 +22,35 @@ function iteracja(vertex::Wierzcholek{})
   return true;
 end
 
+function is_neighbour(v1::ASCIIString, v2::ASCIIString, edge_list::Array{Krawedz})
+  for edge in edge_list
+    if(edge.v1 == v1 && edge.v2 == v2)
+        println("edge.v1 = $(edge.v1), v1 = $(v1), edge.v2 = $(edge.v2), v2 = $(v2)");
+      return true;
+    end
+  end
+  return false;
+end
+
 function iteracja_krawedzi2(edge_list::Array{Krawedz})
 
   n = length(edge_list);
   dist = eye(n);
   prev = eye(n);
-  vertex_list::Array{ASCIIString}
+  vertex_list = [];
 
-  for i in eachindex(edge_list)
-    push!(vertex_list, edge_list.v1);
+  for edge in edge_list
+    push!(vertex_list, edge.v1, edge.v2);
+  end
+
+  vertex_list = unique(vertex_list);
+
+  for vertex1 in vertex_list
+    for vertex2 in vertex_list
+      if is_neighbour(vertex1, vertex2, edge_list)
+        println("$(vertex1) is neighbour of $(vertex2)");
+      end
+    end
   end
 
     for u in eachindex(vertex_list)
@@ -55,8 +75,8 @@ function iteracja_krawedzi2(edge_list::Array{Krawedz})
       end
     end
 
-println("dist =\n $(dist)");
-println("prev =\n $(prev)");
+# println("dist =\n $(dist)");
+# println("prev =\n $(prev)");
 println("vertex_list =\n $(vertex_list)");
 
   return true;
@@ -69,6 +89,7 @@ edge_list = [
 Krawedz("a","b",1)
 Krawedz("b","c",2)
 Krawedz("a","c",4)];
- @test iteracja_krawedzi2(vertex_list, edge_list) == true
+ @test iteracja_krawedzi2(edge_list) == true
 
  println("Test zakończony sukcesem4");
+ println(time());
